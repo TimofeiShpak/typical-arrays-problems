6b0f31537b83438409698541ff8abbcfbdfb681c
TimofeiShpak <60565887+TimofeiShpak@users.noreply.github.com>
2020-02-21 09:47:37 +0400

    docs: update file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+	if(!Array.isArray(array))return 0;
+	if(!array.length)return 0;
+	let min=0;
+  for(let i=0; i<array.length; i++){
+  	min = Math.min(array[i],min);
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+	if(!Array.isArray(array))return 0;
+	if(!array.length)return 0;
+  let max=0;
+  for(let i=0; i<array.length; i++){
+  	max = Math.max(array[i],max);
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if(!Array.isArray(array))return 0;
+	if(!array.length)return 0;
+  let avg=0;
+  for(let i=0; i<array.length; i++){
+  	avg+=array[i];
+  }
+  avg=avg/array.length;
+  return avg;
 }
